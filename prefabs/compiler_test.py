44c8a0fd1fa6b896ffcc9ad603250cafacf27dc1
anson <bridges.anson@gmail.com>
2016-02-07 12:45:55 -0800

    how can i get a variable from another program?

--- prefabs/compiler_test.py
+++ prefabs/compiler_test.py
@@ -6,41 +6,40 @@ global values
 global beg_template
 
 open('Script_.tmp', 'w').close()
-def service_func():
-    beg_template = """
-    versioninfo
-    {
-	"editorversion" "400"
-	"editorbuild" "7026"
-	"mapversion" "1"
-	"formatversion" "100"
-	"prefab" "0"
-    }
-    visgroups
-    {
-    }
-    viewsettings
-    {
-	"bSnapToGrid" "1"
-	"bShowGrid" "1"
-	"bShowLogicalGrid" "0"
-	"nGridSpacing" "64"
-	"bShow3DGrid" "0"
-    }
-    world
-    {
-	"id" "1"
-	"mapversion" "1"
-	"classname" "worldspawn"
-	"skyname" "sky_tf2_04"
-	"maxpropscreenwidth" "-1"
-	"detailvbsp" "detail_2fort.vbsp"
-	"detailmaterial" "detail/detailsprites_2fort"
-    """
+
+beg_template = """
+versioninfo
+{
+    "editorversion" "400"
+    "editorbuild" "7026"
+    "mapversion" "1"
+    "formatversion" "100"
+    "prefab" "0"
+}
+visgroups
+{
+}
+viewsettings
+{
+    "bSnapToGrid" "1"
+    "bShowGrid" "1"
+    "bShowLogicalGrid" "0"
+    "nGridSpacing" "64"
+    "bShow3DGrid" "0"
+}
+world
+{
+    "id" "1"
+    "mapversion" "1"
+    "classname" "worldspawn"
+    "skyname" "sky_tf2_04"
+    "maxpropscreenwidth" "-1"
+    "detailvbsp" "detail_2fort.vbsp"
+    "detailmaterial" "detail/detailsprites_2fort"
+"""
 if __name__ == '__main__':
     # service.py executed as script
     # do something
-    service_func()
     wall_prefab.find()
-    whole = beg_template + values
+    whole = beg_template + #values from wall_prefab, how to?
 
