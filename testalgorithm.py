1635755491e879bada240716c982ac7fa883b9de
anson <bridges.anson@gmail.com>
2016-02-06 22:48:32 -0800

    slightly better algorithm
    , now wont bug out of there's a 'z' somewhere else.

--- testalgorithm.py
+++ testalgorithm.py
@@ -8,11 +8,13 @@ open('Srcipt_.tmp', 'w').close()
 f = open('prefabs\prefab_blanktile.txt', 'r+')
 lines = f.readlines()
 
-strings = ('z')
+strings = ('z3','z4','z5','z6')
 
 for line in lines:
     if any(s in line for s in strings):
         looplist += '1'
+
+print(looplist)
 for i in range(len(looplist)-1):
     values.append(lines[start])
     start += 11
