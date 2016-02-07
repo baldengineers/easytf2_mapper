5b226f7a0b36c97c1024f4cd4b3595d35bb27d61
jony1266 <liu.z.jonathan@gmail.com>
2016-02-07 11:33:56 -0800

    Added the menu bar
    menu bar includes: exit, open, save, and new

--- main.py
+++ main.py
@@ -5,12 +5,48 @@ from PySide.QtGui import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        #create the main window
         super(MainWindow, self).__init__()
-        self.setGeometry(50,50,500,300)
+        self.setGeometry(500, 300, 500, 300)
         self.setWindowTitle("Easy TF2 Mapper")
         self.setWindowIcon(QIcon("map.ico"))
+
+        #create menubar
+        exitAction = QAction("&Exit", self)
+        exitAction.setShortcut("Ctrl+Q")
+        exitAction.setStatusTip("Exit Application")
+        exitAction.triggered.connect(self.close_application)
+
+        openAction = QAction("&Open", self)
+        openAction.setShortcut("Ctrl+O")
+        openAction.setStatusTip("Open .vmf file")
+        #openAction.triggered.connect()
+
+        saveAction = QAction("&Save", self)
+        saveAction.setShortcut("Ctrl+S")
+        saveAction.setStatusTip("Save File as .txt")
+        #saveAction.triggered.connect()
+
+        newAction = QAction("&New", self)
+        newAction.setShortcut("Ctrl+n")
+        newAction.setStatusTip("Create a New File")
+        #newAction.triggered.connect()
+
+        self.statusBar()
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu("&File")
+        fileMenu.addAction(exitAction)
+        fileMenu.addAction(openAction)
+        
+        self.home()
+
+    def home(self):
         self.show()
 
+    def close_application(self):
+        sys.exit()
+
 app = QApplication(sys.argv)
-GUI = MainWindow()
+gui = MainWindow()
 sys.exit(app.exec_())
