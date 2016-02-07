f7419bc06fdb279890fc68f9192f1d9104542f6e
jony1266 <liu.z.jonathan@gmail.com>
2016-02-07 10:55:38 -0800

    Added gui to main.py
    Yes, I did it.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 #use blocks to create maps
+import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setGeometry(50,50,500,300)
+        self.setWindowTitle("Easy TF2 Mapper")
+        self.setWindowIcon(QIcon("map.ico"))
+        self.show()
+
+app = QApplication(sys.argv)
+GUI = MainWindow()
+sys.exit(app.exec_())
