279191a2699abd93992d785e0b6572a6b9bfc6ae
anson <bridges.anson@gmail.com>
2016-02-07 11:57:27 -0800

    added a compiler test (not working at all)
    please halp

---
+++ prefabs/compiler_test.py
@@ -0,0 +1,46 @@
+import os
+import wall_prefab
+import ground_prefab
+
+global values
+global beg_template
+
+open('Script_.tmp', 'w').close()
+def service_func():
+    beg_template = """
+    versioninfo
+    {
+	"editorversion" "400"
+	"editorbuild" "7026"
+	"mapversion" "1"
+	"formatversion" "100"
+	"prefab" "0"
+    }
+    visgroups
+    {
+    }
+    viewsettings
+    {
+	"bSnapToGrid" "1"
+	"bShowGrid" "1"
+	"bShowLogicalGrid" "0"
+	"nGridSpacing" "64"
+	"bShow3DGrid" "0"
+    }
+    world
+    {
+	"id" "1"
+	"mapversion" "1"
+	"classname" "worldspawn"
+	"skyname" "sky_tf2_04"
+	"maxpropscreenwidth" "-1"
+	"detailvbsp" "detail_2fort.vbsp"
+	"detailmaterial" "detail/detailsprites_2fort"
+    """
+if __name__ == '__main__':
+    # service.py executed as script
+    # do something
+    service_func()
+    wall_prefab.find()
+    whole = beg_template + values
+
