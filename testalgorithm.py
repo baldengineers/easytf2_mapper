f7a59194f5f3aee641bbb409a7885afc21f7eda9
anson <bridges.anson@gmail.com>
2016-02-06 22:42:34 -0800

    made it pliable

--- testalgorithm.py
+++ testalgorithm.py
@@ -2,12 +2,18 @@
 
 import os
 start = 6
+looplist = '1'
 values=[]
 open('Srcipt_.tmp', 'w').close()
 f = open('prefabs\prefab_blanktile.txt', 'r+')
 lines = f.readlines()
 
-for i in range(6):
+strings = ('z')
+
+for line in lines:
+    if any(s in line for s in strings):
+        looplist += '1'
+for i in range(len(looplist)-1):
     values.append(lines[start])
     start += 11
         
