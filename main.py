58b4f8a5ad831e51d017cf52fc98590ec29ed03a
jony1266 <liu.z.jonathan@gmail.com>
2016-02-07 22:13:16 -0800

    why the heck do i git errorh?
    plz help

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
-class MainWindow(QMainWindow):
+class GridBtn():
     def __init__(self):
+        self.button = QPushButton("0", self)
+
+    def change_val(self, val):
+        self.button = QPushButton(val, self)
+
+class MainWindow(QMainWindow):
+    def __init__(self, app):
         #create the main window
         super(MainWindow, self).__init__()
         self.setGeometry(500, 300, 500, 300)
@@ -20,12 +27,12 @@ class MainWindow(QMainWindow):
         openAction = QAction("&Open", self)
         openAction.setShortcut("Ctrl+O")
         openAction.setStatusTip("Open .vmf file")
-        #openAction.triggered.connect()
+        openAction.triggered.connect(self.file_open)
 
         saveAction = QAction("&Save", self)
         saveAction.setShortcut("Ctrl+S")
         saveAction.setStatusTip("Save File as .vmf")
-        #saveAction.triggered.connect()
+        saveAction.triggered.connect(self.file_save)
 
         newAction = QAction("&New", self)
         newAction.setShortcut("Ctrl+n")
@@ -41,15 +48,64 @@ class MainWindow(QMainWindow):
         fileMenu.addAction(saveAction)
         fileMenu.addAction(exitAction)
         
-        
         self.home()
 
+    def closeEvent(self, event):
+        #closeEvent runs close_application when the x button is pressed
+        event.ignore()
+        self.close_application()
+        
     def home(self):
+        self.texture_list = QListWidget()
+        self.texture_list.addItem("texture")
+        
+        self.button_grid_layout = QGridLayout()
+        #TODO: Manually set grid x and grid y
+        self.grid_x = 3
+        self.grid_y = 3
+        
+        for x in range(self.grid_x):
+            for y in range(self.grid_y):
+                grid_btn = GridBtn()
+                self.button_grid_layout.addWidget(grid_btn.button,x,y)
+        
+        self.column = QHBoxLayout()
+        self.column.addWidget(self.texture_list)
+        self.column.addLayout(self.button_grid_layout)
         self.show()
 
+    def file_open(self):
+        name = QFileDialog.getOpenFileName(self, "Open File", "C:/","*.vmt")
+        file = open(name, "r")
+        lines = []
+
+        for line in file:
+            lines.append(line)
+            for letter in line:
+                #TODO: Use Anson's functions to decipher the vmt file, to then open
+                #the file into the program
+                pass
+        
+        #with file:
+            #text = file.read()
+            
+    def file_save(self):
+        name = QFileDialog.getSaveFileName(self, "Save File", "//", "*.vmt")
+        file = open(name, "w")
+        text = self.textEdit.toPlainText()
+        file.write(text)
+        file.close()
+
     def close_application(self):
-        sys.exit()
+        choice = QMessageBox.question(self, "Exit",
+                                      "Are you sure you want to exit?",
+                                      QMessageBox.Yes | QMessageBox.No,
+                                      QMessageBox.No)
+        if choice == QMessageBox.Yes:
+            sys.exit()
+        else:
+            pass
 
 app = QApplication(sys.argv)
-gui = MainWindow()
-sys.exit(app.exec_())
+gui = MainWindow(app)
+app.exec_()
