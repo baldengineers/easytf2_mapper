34e57cbd676c10c98aad94efa45bc6d924c70c4a
jony1266 <liu.z.jonathan@gmail.com>
2016-02-12 21:44:10 -0800

    Updated createPrefab.py
    Made it so that the variables accept a wide range of values, including
    negatives

--- createPrefab.py
+++ createPrefab.py
@@ -3,6 +3,7 @@ num_list = []
 value_list = []
 
 var_num = 1
+value_list_history = []
 name = "prefabs\godplsno.vmf"
 file = open(name, "r")
 
@@ -30,6 +31,7 @@ def write_var():
 
   print("value_list: ", value_list)
   value_list_history.append(value_list)
+  print("value_list_history: ", value_list_history)
   xyz_list = ["x", "y", "z"]
   
   for var in xyz_list:
@@ -43,15 +45,24 @@ def write_var():
         
     if var == "z":
       file_list.append("%s%d = %d" %(var, var_num, value))
+      print(file_list)
     elif value == 0:
       file_list.append("%s%d = %s*%s512" %(var, var_num, var, negative))
       print(file_list)
-    elif value == 512:
-      file_list.append("%s%d = %s*%s512 + 512" %(var, var_num, var, negative))
+    else:
+      file_list.append("%s%d = %s*%s512 + (%d)" %(var, var_num, var, negative, value))
       print(file_list)
-    elif value == -512:
-      file_list.append("%s%d = %s*%s512 - 512" %(var, var_num, var, negative))
-      print(file_list)
+  
+
+    #elif value == 0:
+      #file_list.append("%s%d = %s*%s512" %(var, var_num, var, negative))
+      #print(file_list)
+    #elif value == 512:
+      #file_list.append("%s%d = %s*%s512 + 512" %(var, var_num, var, negative))
+      #print(file_list)
+    #elif value == -512:
+      #file_list.append("%s%d = %s*%s512 - 512" %(var, var_num, var, negative))
+      #print(file_list)
 
 for line in openlines:
   if "(" in line:
@@ -63,6 +74,8 @@ for line in openlines:
       except ValueError:
         if letter == " ":
           num_list.append("SEPARATE")
+        elif letter == "-":
+          num_list.append("-")
         elif letter == ")":
           write_var()
           var_num += 1
