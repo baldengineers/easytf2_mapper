f7a72b2c63b3895dc583f34d1d9be367ecb881df
jony1266 <liu.z.jonathan@gmail.com>
2016-02-16 11:09:18 -0800

    Fixed the error, but a new speculation has arisen
    For the prefabs that have two solids, such as the wall prefabs that have
    both the ground and the wall. there are two "world_id_num"s in the txt
    file, so when the prefab code replaces them, they become the same exact
    world id num in the vmf file. However, this does not affect the vmf
    opening in hammer for some reason.

--- ground_prefab.py
+++ ground_prefab.py
@@ -4,7 +4,7 @@ import os
 def createTile(posx, posy, id_num, world_id_num):
     looplist = '1'
     values=[]#Values are all of the lines of a prefab that have the vertex coords
-    f = open('prefab_template\prefab_blanktile.txt', 'r+')
+    f = open('prefab_template\ground_prefab.txt', 'r+')
     lines = f.readlines() #gathers each line of the prefab and puts numbers them
 
     x1 = posx*512
