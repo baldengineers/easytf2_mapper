09bc14a71b58af88ef548b7f9cef795048e2afee
toads <bridges.anson@gmail.com>
2016-03-03 23:15:21 -0800

    skybox stuff (incomplete by a lot)

---
+++ generateSkybox.py
@@ -0,0 +1,10 @@
+import os
+
+def createSkyboxLeft(gridx,gridy,skyboxz,id_num,world_id_num):
+    values = []
+    f = open('prefab_template/skybox_worldgeo/skybox_side.txt','r+')
+    lines = f.readlines()
+
+    x1 = 0
+    y1 = 0
+    skyboxz =
