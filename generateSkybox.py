fbdb13e75c607c4c3bd9f08034b712cc1321d61f
toads <bridges.anson@gmail.com>
2016-03-04 16:36:30 -0800

    added (untested) generation of skybox_left

--- generateSkybox.py
+++ generateSkybox.py
@@ -4,7 +4,15 @@ def createSkyboxLeft(gridx,gridy,skyboxz,id_num,world_id_num):
     values = []
     f = open('prefab_template/skybox_worldgeo/skybox_side.txt','r+')
     lines = f.readlines()
+    values = "".join(lines)#converting list to string
+    ogvalues = "".join(lines)
+    values = values.replace("world_idnum", str(world_id_num))
+    world_id_num += 1
+    values = values.replace("skyboxz",str(skyboxz))
+    values = values.replace("ynum",str(gridy*-512))
 
-    x1 = 0
-    y1 = 0
-    skyboxz =
+    for i in range(ogvalues.count('id_num')):
+        values = values.replace('id_num', str(id_num), 1)
+        id_num = id_num+1
+
+    return values, id_num, world_id_num
