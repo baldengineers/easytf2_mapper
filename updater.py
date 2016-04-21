3e8e59b480ebbec0c27b431545fe7c883df074c1
toads <bridges.anson@gmail.com>
2016-04-21 16:15:38 -0700

    made updater work.

--- updater.py
+++ updater.py
@@ -1,12 +1,30 @@
 #easy tf2 mapper updater!!
-import urllib.request
-import urllib.parse
+import os
+import requests
 import zipfile
+import shutil
+import urllib.request
 
 try:
+    print('cleaning directories...')
+    for f in os.listdir('updated/'):
+        try:
+            os.remove('updated/'+f)
+        except:
+            shutil.rmtree('updated/'+f)
+    #
+    #gets file info
+    #
+    url = 'http://github.com/baldengineers/easytf2_mapper/archive/master.zip'
+    site = urllib.request.urlopen(url)
+    meta = site.info()
+    size = meta["Content-Length"]
+    print(size)
+    
     #
     #downloads zip
     #
+    '''
     url = 'http://github.com/baldengineers/easytf2_mapper/archive/master.zip'
     headers = {}
     headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
@@ -14,31 +32,49 @@ try:
     resp = urllib.request.urlopen(req) 
     respData = resp.read()             
 
-    saveFile = open('leveltemp/latest.zip', 'w') 
+    saveFile = open('leveltemp/easytf2mapper-master.zip', 'w') 
     saveFile.write(str(respData))
     saveFile.close()
+    '''
+    with open('leveltemp/easytf2mapper-master.zip', 'wb') as handle:
+        response = requests.get('http://github.com/baldengineers/easytf2_mapper/archive/master.zip', stream=True)
 
+        if not response.ok:
+            print('something went wrong getting the file.')
+
+        for i,block in enumerate(response.iter_content(1024)):
+            print("\n"*50+"%d/%d bytes downloaded (%d percent)" % (i*1024, int(size), (float((i*1024)/int(size))*100)))
+            handle.write(block)
+    
+    print('downloading complete.')
     #
     #unpacks master zip
     #
-    zipupdate = zipfile.ZipFile('tmp/latest.zip', 'r+')
-    zipupdate.extract('latestwinredist/easytf2mapper_latest.zip','updated/')
-    zipupdate.close()
 
+    zipupdate = zipfile.ZipFile('leveltemp/easytf2mapper-master.zip', 'r')
+    for file in zipupdate.namelist():
+        if file.startswith('easytf2_mapper-master/latestwinredist'):
+            zipupdate.extract(file, 'updated/')
+    zipupdate.close()
+    print('done unpacking master zip')
+    
     #
     #unpacks latest zip
     #
-    ziplatest = zipfile.ZipFile('updated/easytf2mapper_latest.zip')
+    ziplatest = zipfile.ZipFile('updated/easytf2_mapper-master/latestwinredist/easytf2mapper_latest.zip')
     ziplatest.extractall('updated/')
     ziplatest.close()
-
+    print('done unpacking latest zip')
+    
     #removes zip after it's done
-    os.remove('updated/easytf2mapper_latest.zip')
+    os.remove('leveltemp/easytf2mapper-master.zip')
+    shutil.rmtree('updated/easytf2_mapper-master/')
 
     #status
     print("Update successful. You can find the updated version in the /updated directory.")
 except Exception as e:
     #status if errors
-    print(str(e))
+    print('ERROR!\n',str(e))
 #gives status
+
 
